d2ac18712f3cb94d1309f08733b642348463c701
jiwoolee <jnh0465@naver.com>
2018-08-21 01:08:50 +0900

    style: Modify dynamodb index

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var fcm_push = require('./push_notification.js'); //push_notification.js 끌어오기
 							
 var AWS = require("aws-sdk");			   // --> npm install aws-sdk --save
+var dynamodb = require('./payment_dynamodb_query.js');	   //dynamodb_query.js
 var docClient = new AWS.DynamoDB.DocumentClient();
 AWS.config.update({
 	region: "us-east-1",
@@ -17,23 +18,25 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/payment',function(req,res){				
-	var i=20180819003;						     // 일단 주문번호(id)로 dynamodb 쿼리
+	var id=15347716504970820;					     // 일단 주문번호(id)로 dynamodb 쿼리
 	dynamodb.send(i);						     // to dynamodb_query.js
 
 	setTimeout(function() {						     // 쿼리 할 시간 확보 위해 1초정도 지연함. 효과가 있는지는 의문
-		var id = dynamodb.id;					     // 퀴리된 값 가져옴
-		var name = dynamodb.name;
-		var amount = dynamodb.amount;
-		var tel = dynamodb.tel;
+		var id = dynamodb.merchant_uid;				     // 퀴리된 값 가져와서 각 변수에 저장
+		var tamount = dynamodb.total_amount;
+		var tel = dynamodb.buyer_tel;
+		var name = dynamodb.market_name;
+		var time = dynamodb.required_time;
 		//console.log(id+name+amount+tel);
 
 		res.render(__dirname+'/public/payment_button.html',{         // to payment_button.html
 		//res.sendFile(__dirname+'/public/payment_button.html',{
 		//res.render('test',{
 			'merchant_uid' : id,
-			'name' : name,
-			'amount' : amount,
-			'buyer_tel' : tel
+			'total_amount' : tamount,
+			'buyer_tel' : tel,
+			'market_name' : name,
+			'required_time' : time
 		})
 	}, 1000);
 });
